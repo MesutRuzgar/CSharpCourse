11f10417f8079ba072855a9d2830b5754ed4f1b7
MesutRuzgar <mesutruzgar@gmail.com>
2022-02-25 16:07:28 +0300

    method overloading

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -21,13 +21,15 @@ namespace Methods
             //out keyword de yukarıda herhangi bir değer set etmek zorunda değiliz
             var result2 = Add3(out number1, number2);
 
-            Console.WriteLine(result2);
+            //Console.WriteLine(result2);
 
             //şuan 20 veriyor number1 ancak ref keyword kullanırsak değeri
             //method içindekine dönüşüyor
             //kullanıcıya göre kredi hesaplarken kullanabiliriz mesela
 
-            Console.WriteLine(number1);
+            //Console.WriteLine(number1);
+
+            Console.WriteLine(Multiply(2,4,5));
             Console.ReadLine();
         }
 
@@ -48,6 +50,17 @@ namespace Methods
         {
             number1 = 30;
             return number1 + number2;
+        }
+
+        static int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        //method aynı isimde olsa bile parametresi farklı olduğundan sorun çıkmaz
+        static int Multiply(int number1, int number2,int number3)
+        {
+            return number1 * number2* number3;
         }
     }
 }
