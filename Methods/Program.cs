34ea32b6ad853d5cafa657d83cf50526ad8600fb
MesutRuzgar <mesutruzgar@gmail.com>
2022-02-25 15:55:41 +0300

    ref keyword challenge

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -10,9 +10,21 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            Add();
-            var result = Add2(20);
-            Console.WriteLine(result);
+            //Add();
+            //var result = Add2(20);
+            //Console.WriteLine(result);
+
+            int number1 = 20;
+            int number2 = 100;
+            var result2 = Add3(ref number1, number2);
+
+            Console.WriteLine(result2);
+
+            //şuan 20 veriyor number1 ancak ref keyword kullanırsak değeri
+            //method içindekine dönüşüyor
+            //kullanıcıya göre kredi hesaplarken kullanabiliriz mesela
+
+            Console.WriteLine(number1);
             Console.ReadLine();
         }
 
@@ -20,13 +32,19 @@ namespace Methods
         {
             Console.WriteLine("Added!");
         }
-        
+
         //default değerler her zaman methodun sonunda olur 1 adet default değerin var ise.
-        static int Add2(int number1, int number2=30)
+        static int Add2(int number1, int number2 = 30)
         {
             var result = number1 + number2;
             return result;
-           
+
+        }
+
+        static int Add3(ref int number1, int number2)
+        {
+            number1 = 30;
+            return number1 + number2;
         }
     }
 }
