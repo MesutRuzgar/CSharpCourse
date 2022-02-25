6d1489a5745eb63b5e7d767884ca5a4589c2ed22
MesutRuzgar <mesutruzgar@gmail.com>
2022-02-25 14:38:28 +0300

    long value type

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -13,12 +13,24 @@ namespace TypesAndVariables
             //Console.WriteLine("Hello World!");
 
             //int veri tipinin alabileceği max ve min rakamlar
+            //32 byte yer kaplar
 
             int number1 = -2147483648;
             int number2 = 2147483647;
 
-            Console.WriteLine("Number1 is {0}",number2);
+            //long veri tipi int veri tipini içinde barındırır.
+            //long sınırı ise 19 karakterden oluşur
+            //64 byte yer kaplar
+            //long veri tipinin alabileceği max ve min rakamlar
+
+            long number3 = -9223372036854775808;
+            long number4 = 9223372036854775807;
+
+            Console.WriteLine("Number1 is {0}",number4);
             Console.ReadLine();
+
+
+
         }
     }
 }
