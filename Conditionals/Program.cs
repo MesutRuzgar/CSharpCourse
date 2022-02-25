4598248a9b4459d676ab2c450b6b5b5e4e6a6222
MesutRuzgar <mesutruzgar@gmail.com>
2022-02-25 15:30:42 +0300

    demo conditionals

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,7 +10,7 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            var number = 20;
+            var number = 200;
 
             //single line if 
             //Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
@@ -29,18 +29,33 @@ namespace Conditionals
             //    Console.WriteLine("Number is not 10 or 20");
             //}
 
-            switch (number)
+            //switch
+            //switch (number)
+            //{
+            //    case 10:
+            //        Console.WriteLine("Number is 10");
+            //        break;
+            //    case 20:
+            //        Console.WriteLine("Number is 20");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Number is not 10 or 20");
+            //        break;
+            //}
+
+            if (number >= 0 && number <= 100)
             {
-                case 10:
-                    Console.WriteLine("Number is 10");
-                    break;
-                case 20:
-                    Console.WriteLine("Number is 20");
-                    break;
-                default:
-                    Console.WriteLine("Number is not 10 or 20");
-                    break;
+                Console.WriteLine("Number is between 0-100");
             }
+            else if (number > 100 && number <= 200)
+            {
+                Console.WriteLine("Number is between 101-200");
+            }
+            else if (number > 200 || number < 0)
+            {
+                Console.WriteLine("Number is less than 0 or greater than 200");
+            }
+
 
             Console.ReadLine();
         }
