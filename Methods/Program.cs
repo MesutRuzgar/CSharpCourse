c186e96248472ee5fe9fbf64516f499193e91cd2
MesutRuzgar <mesutruzgar@gmail.com>
2022-02-25 16:14:00 +0300

    params method example

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -29,7 +29,9 @@ namespace Methods
 
             //Console.WriteLine(number1);
 
-            Console.WriteLine(Multiply(2,4,5));
+            //Console.WriteLine(Multiply(2,4,5));
+
+            Console.WriteLine(Add4(1, 2, 3, 4, 5, 6));
             Console.ReadLine();
         }
 
@@ -62,5 +64,13 @@ namespace Methods
         {
             return number1 * number2* number3;
         }
+
+        //birden fazla parametre varsa bu şekilde sürekli overload yapmaktansa yazabiliriz.
+        //params parametresi birden fazla parametre var ise en sonda olmak zorunda
+        static int Add4(params int[] numbers)
+        {
+            return numbers.Sum();
+        }
+      
     }
 }
