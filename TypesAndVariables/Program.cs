f068d084c564969059968bd7ce9c1abd9ec0784b
MesutRuzgar <mesutruzgar@gmail.com>
2022-02-25 14:40:02 +0300

    short value type

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -26,6 +26,12 @@ namespace TypesAndVariables
             long number3 = -9223372036854775808;
             long number4 = 9223372036854775807;
 
+            //short veri tipi 16 byte yer kaplar
+            //short veri tipinin alabileceği max ve min rakamlar
+
+            short number5 = -32768;
+            short number6 = 32767;
+
             Console.WriteLine("Number1 is {0}",number4);
             Console.ReadLine();
 
