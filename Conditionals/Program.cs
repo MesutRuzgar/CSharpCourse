0febaa65adbe6006087d55a0d08ae6b7acfc825d
MesutRuzgar <mesutruzgar@gmail.com>
2022-02-25 15:24:22 +0300

    switch example

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -15,18 +15,31 @@ namespace Conditionals
             //single line if 
             //Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
 
-            if (number == 10)
-            {
-                Console.WriteLine("Number is 10");
+            //if (number == 10)
+            //{
+            //    Console.WriteLine("Number is 10");
 
-            }
-            else if (number==20)
+            //}
+            //else if (number==20)
+            //{
+            //    Console.WriteLine("Number is 20");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Number is not 10 or 20");
+            //}
+
+            switch (number)
             {
-                Console.WriteLine("Number is 20");
-            }
-            else
-            {
-                Console.WriteLine("Number is not 10 or 20");
+                case 10:
+                    Console.WriteLine("Number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("Number is 20");
+                    break;
+                default:
+                    Console.WriteLine("Number is not 10 or 20");
+                    break;
             }
 
             Console.ReadLine();
