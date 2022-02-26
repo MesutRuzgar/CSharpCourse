ae2acf8dcf181b55a9e8d8513cc8f82cf943ceea
MesutRuzgar <mesutruzgar@gmail.com>
2022-02-26 11:51:45 +0300

    edit

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -11,9 +11,22 @@ namespace Loops
         static void Main(string[] args)
         {
             //ForLoop();
+            // ForLoop2();
             //WhileLoop();
+            // DoWhileLoop();
 
 
+
+
+
+
+
+
+        }
+
+
+        private static void DoWhileLoop()
+        {
             int number = 10;
             do
             {
@@ -24,18 +37,7 @@ namespace Loops
                 //While koşulu sağlanmasa bile kod 1 kez çalışır
                 //örnek olarak data yoksa sayfayı kapat olabilir
 
-            } while (number>=11);
-            Console.ReadLine();
-
-
-
-
-            //burası da tersten yazım şekli 100 den başlayıp 2 azaltıyor 0 olunca bitiyor.
-
-            for (int i = 100; i >= 0; i = i - 2)
-            {
-                Console.WriteLine(i);
-            }
+            } while (number >= 11);
             Console.ReadLine();
         }
 
@@ -49,16 +51,24 @@ namespace Loops
                 number--;
 
             }
-        }
-
-        //i 1 den başlıcak; 100 e kadar ve 100 dahil;
-        //i++ her döngü bittiğinde 1 arttıracak demek;
-        //i=i+2 de yazabiliriz 2 arttır demek
+        }     
 
         private static void ForLoop()
         {
+            //i 1 den başlıcak; 100 e kadar ve 100 dahil;
+            //i++ her döngü bittiğinde 1 arttıracak demek;
+            //i=i+2 de yazabiliriz 2 arttır demek
             for (int i = 1; i <= 100; i++)
             {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+        private static void ForLoop2()
+        {
+            for (int i = 100; i >= 0; i = i - 2)
+            {
+                //burası da tersten yazım şekli 100 den başlayıp 2 azaltıyor 0 olunca bitiyor.
                 Console.WriteLine(i);
             }
             Console.ReadLine();
