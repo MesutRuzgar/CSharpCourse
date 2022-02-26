8f5893a3de8b3a5f346eb6732e7cdf753e2b0e91
MesutRuzgar <mesutruzgar@gmail.com>
2022-02-26 16:31:53 +0300

    Virtiual method example

---
+++ VirtualMethods/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethods
+{
+    class Program
+    {
+
+        //virtual yapmamızın sebebi aslında her yerde aynı kullanılabilecek
+        //kodu olurda başka yerde aynı methodu farklı kodlarla
+        //çalıştırmamız gerekirse diye override etmemize yani ezmemize
+        //yarıyor.
+
+        static void Main(string[] args)
+        {
+            SqlServer sqlServer = new SqlServer();
+            sqlServer.Add();
+            MySqlServer mySqlServer = new MySqlServer();
+            mySqlServer.Add();
+            Console.ReadLine();
+        }
+
+        class Database
+        {
+            public virtual void Add()
+            {
+                Console.WriteLine("Added by default");
+            }
+            public virtual void Delete()
+            {
+                Console.WriteLine("Deleted by default");
+            }
+        }
+        class SqlServer:Database
+        {
+            public override void Add()
+            {
+                Console.WriteLine("Added by Sql Code");
+               // base.Add();
+            }
+        }
+
+        class MySqlServer:Database
+        {
+
+        }
+    }
+}
