0d37e1bdedacfc9a9fdb1f18b72eebbbe8b96fd7
MesutRuzgar <mesutruzgar@gmail.com>
2022-02-26 15:18:03 +0300

    Interface started

---
+++ Interfaces/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        //soyut nesne
+        interface IPerson
+        {
+            int Id { get; set; }
+            string FirstName { get; set; }
+            string LastName { get; set; }
+
+        }
+
+        //somut nesne
+        class Customer : IPerson
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public string Adress { get; set; }
+        }
+
+        class Student : IPerson
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+            public string  Departmant { get; set; }
+        }
+    }
+}
