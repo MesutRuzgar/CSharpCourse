79a96a9b01f441a6c72e3c05b3fc7c4e8b52fa4e
MesutRuzgar <mesutruzgar@gmail.com>
2022-02-26 11:44:35 +0300

    While loop

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -10,25 +10,42 @@ namespace Loops
     {
         static void Main(string[] args)
         {
-            //i 1 den başlıcak; 100 e kadar ve 100 dahil;
-            //i++ her döngü bittiğinde 1 arttıracak demek;
-            //i=i+2 de yazabiliriz 2 arttır demek
+            //ForLoop();
 
-            for (int i = 1; i <= 100; i++)
+
+            int number = 100;
+            while (number >= 0)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(100);
+                //whilede koda bu şekilde müdahale etmez isek kod sonsuza kadar çalışır.
+                number--;
+
             }
-            Console.ReadLine();
+
+
+
+
 
             //burası da tersten yazım şekli 100 den başlayıp 2 azaltıyor 0 olunca bitiyor.
 
-            for (int i = 100; i >= 0; i=i-2)
+            for (int i = 100; i >= 0; i = i - 2)
             {
                 Console.WriteLine(i);
             }
             Console.ReadLine();
         }
 
+        //i 1 den başlıcak; 100 e kadar ve 100 dahil;
+        //i++ her döngü bittiğinde 1 arttıracak demek;
+        //i=i+2 de yazabiliriz 2 arttır demek
 
+        private static void ForLoop()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
     }
 }
