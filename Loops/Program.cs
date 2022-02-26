6b307ca302232d0daa7b97009cd250e49c9bf7d2
MesutRuzgar <mesutruzgar@gmail.com>
2022-02-26 11:36:40 +0300

    For Loop

---
+++ Loops/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //i 1 den başlıcak; 100 e kadar ve 100 dahil;
+            //i++ her döngü bittiğinde 1 arttıracak demek;
+            //i=i+2 de yazabiliriz 2 arttır demek
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+            //burası da tersten yazım şekli 100 den başlayıp 2 azaltıyor 0 olunca bitiyor.
+
+            for (int i = 100; i >= 0; i=i-2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+
+
+    }
+}
