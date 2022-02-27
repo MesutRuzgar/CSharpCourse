0dcdbfaac7f343886d218c272fe454d5a766dcb6
MesutRuzgar <mesutruzgar@gmail.com>
2022-02-27 13:48:04 +0300

    type safe collection example

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -11,6 +11,37 @@ namespace Collections
     {
         static void Main(string[] args)
         {
+            //ArrayList();
+
+            //type safe example. sadece string ile çalışıyoruz burada onu belirttik.
+            List<string> cities = new List<string>();
+            cities.Add("Ankara");
+            cities.Add("İstanbul");
+            foreach (var city in cities)
+            {
+                Console.WriteLine(city);
+            }
+            //klasik yöntem
+            List<Customer> customers = new List<Customer>();
+            customers.Add(new Customer { Id = 1, FirstName = "Mesut" });
+            customers.Add(new Customer { Id = 2, FirstName = "Umut" });
+
+            //2. yöntem
+            List<Customer> customers2 = new List<Customer>{
+            new Customer { Id = 1, FirstName = "Mesut" },
+            new Customer { Id = 2, FirstName = "Umut" }
+            };
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.FirstName);
+            }
+
+
+            Console.ReadLine();
+        }
+
+        private static void ArrayList()
+        {
             //eğer çalıştığımız nesnede type safe yoksa yani belirli bir 
             //type yoksa arraylist tercih edilebilir. Belli bir sınır yoktur
             //istediğimiz kadar ekleyebiliriz.
@@ -18,7 +49,7 @@ namespace Collections
             cities.Add("Ankara");
             cities.Add("Adana");
 
-           
+
             cities.Add("İstanbul");
             cities.Add(1);
             cities.Add('a');
@@ -26,9 +57,11 @@ namespace Collections
             {
                 Console.WriteLine(city);
             }
-
-
-            Console.ReadLine();
         }
+    }
+    class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
     }
 }
