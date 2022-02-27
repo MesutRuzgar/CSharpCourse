e74b33f4a7c33b6dd56aa9f203780ef734c5fe54
MesutRuzgar <mesutruzgar@gmail.com>
2022-02-27 15:06:15 +0300

    exceptions started

---
+++ Exceptions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] students = new string[3] { "Mesut", "Umut", "İdris" };
+                //hata verir burası
+                students[3] = "Mehmet";
+
+            }
+            catch (Exception exception)
+            {
+
+                Console.WriteLine(exception.Message);
+            }
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
